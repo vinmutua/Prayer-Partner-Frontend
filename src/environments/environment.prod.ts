54da0686788e80665c4cd5871b855b3d3f41acef
vinmutua <vincentmutua02@gmail.com>
2025-05-08 10:26:57 +0300

    updated allowed origins

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: true,
   apiUrl: 'https://prayer-partner-backend.onrender.com',
   allowedOrigins: [
-    'https://prayer-partners-app.vercel.app',
-    'https://prayer-partners-h2y0e0bhn-musyokis-projects-31dc945d.vercel.app'
+    'prayer-partner-frontend.vercel.app',
+    'prayer-partner-frontend-ccb60dcn8-musyokis-projects-31dc945d.vercel.app'
   ],
   apiTimeout: 30000,
   retryAttempts: 3

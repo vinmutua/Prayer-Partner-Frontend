c9528ce92828e7bf5c572cc801374db343292761
vinmutua <vincentmutua02@gmail.com>
2025-05-08 12:14:32 +0300

    configured cors for apis

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://prayer-partner-backend.onrender.com/api',
+  apiUrl: 'https://prayer-partner-backend.onrender.com',
   allowedOrigins: [
     'prayer-partner-frontend.vercel.app',
     'prayer-partner-frontend-ccb60dcn8-musyokis-projects-31dc945d.vercel.app'
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api',
+  apiUrl: 'http://localhost:8080', // Removed /api
   allowedOrigins: [
     'http://localhost:4200',
     'http://localhost:8080'
